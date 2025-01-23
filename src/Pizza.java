5732d86667b71e2a2ca0272822328d632f5cad27
JofreCoca <jofrecoca@gmail.com>
2025-01-23 12:21:35 +0100

    Create class Pizza

--- src/Pizza.java
+++ src/Pizza.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Pizza {
+    private String size;
+    private String dough;
+    private String toppings;
+    public void setToppings(String toppings) {
+        this.toppings = toppings;
+    }
+    public void setDough(String dough) {
+        this.dough = dough;
+    }
+    public void setSize(String size) {
+        this.size = size;
+    }
 }
